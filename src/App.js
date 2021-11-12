bca20000b2e730651335750ebf8b136f4c38481d
Jair de Souza Junior <jairsjunior@gmail.com>
2021-11-12 19:17:22 +0100

    feature: Add query groups from Microsoft AD and change button text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-// import './App.css';
 import Home from './app/routes/Home';
-// import App from './app/routes/App'
 
 function Application() {
   return (
     <div className="App">
-      {/* <App /> */}
       <Home />
     </div>
   );
